a653fdd3630b547446eec04b01d7399bf3ae0c28
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-09-29 16:00:29 +0330

    add styles of ContactList component

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,19 +1,25 @@
 import styles from "./ContactList.module.css";
+import userImage from "../../assets/images/user.png";
 
 const ContactList = ({ contacts, onDelete }) => {
   return (
-    <>
+    <section className={styles.contactList}>
       {contacts.map((contact) => {
         const { name, email, id } = contact;
         return (
-          <div key={id}>
-            <p>name : {name}</p>
-            <p>email : {email}</p>
+          <div key={id} className={styles.item}>
+            <div className={styles.userInfo}>
+              <img src={userImage} alt="userImage" />
+              <div>
+                <p>name : {name}</p>
+                <p>email : {email}</p>
+              </div>
+            </div>
             <button onClick={() => onDelete(id)}>delete</button>
           </div>
         );
       })}
-    </>
+    </section>
   );
 };
 
